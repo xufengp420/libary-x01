38a1bcc85099b072dc8c9dbe1c0a25746908f74e
xufengp420 <877545130@qq.com>
2017-08-01 15:16:39 +0800

    xufeng
    1.1

--- GeneralFrameworklibary/GeneralFrameworklibary/src/main/java/xufeng/android/generalframework/okhttp/callback/IResponseCallBack.java
+++
@@ -1,8 +0,0 @@
-package xufeng.android.generalframework.okhttp.callback;
-
-/**
- * Created by Administrator on 2016/9/12.
- */
-public interface IResponseCallBack {
-    <T> T Transform(String response, Class<T> classOfT);
-}
--- GeneralFrameworklibary/GeneralFrameworklibary/src/main/java/xufeng/android/generalframework/okhttp/cookie/store/HasCookieStore.java
+++
@@ -1,9 +0,0 @@
-package xufeng.android.generalframework.okhttp.cookie.store;
-
-/**
- * Created by zhy on 16/3/10.
- */
-public interface HasCookieStore
-{
-    CookieStore getCookieStore();
-}
